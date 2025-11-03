e493cecbfd68078bef455e4133d0e98268758d89
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-190.local>
2025-11-02 17:43:13 -0800

    Working on creating better structure for this lab with loops w/ cases

--- main.cpp
+++ main.cpp
@@ -20,58 +20,92 @@ int main() {
   string file = "codes.txt";
   int results[rows][cols][depth];
   string s;
-  vector<string> data_vector;
-  list<string> data_list;
-  set<string> data_set;
+  vector<string> v;
+  list<string> l;
+  set<string> st;
 
   // Race 1: Reading
-  // vector
   for (int i = 0; i < structs; i++) {
-    auto start_v = high_resolution_clock::now();
-    ifstream fin1(file);
-    vector<string> v;
-    while (fin1 >> s) {
-      v.push_back(s);
+    // vector
+    auto start = chrono::high_resolution_clock::now();
+    ifstream fin(file);
+    switch (i) {
+      case 0: {
+        while (fin >> s) {
+          v.push_back(s);
+          auto end = chrono::high_resolution_clock::now();
+          auto duration = duration_cast<microseconds>(end - start);
+          results[0][i][0] = duration.count();
+          break;
+        }
+      }
+      case 1: {
+        // list
+        while (fin >> s) {
+          l.push_back(s);
+          auto end = chrono::high_resolution_clock::now();
+          auto duration = duration_cast<microseconds>(end - start);
+          results[0][i][0] = duration.count();
+          break;
+        }
+      }
+      case 2: {
+        // set
+        while (fin >> s) {
+          st.insert(s);
+          auto end = chrono::high_resolution_clock::now();
+          auto duration = duration_cast<microseconds>(end - start);
+          results[0][i][0] = duration.count();
+          break;
+        }
+      }
     }
-    fin1.close();
-    auto end_v = high_resolution_clock::now();
-    auto duration_v = duration_cast<microseconds>(end_v - start_v);
-
-    // list
-    auto start_l = high_resolution_clock::now();
-    ifstream fin2(file);
-    list<string> l;
-    while (fin2 >> s) {
-      l.push_back(s);
-    }
-    fin2.close();
-    auto end_l = high_resolution_clock::now();
-    auto duration_l = duration_cast<microseconds>(end_l - start_l);
-
-    // set
-    auto start_s = high_resolution_clock::now();
-    ifstream fin3(file);
-    set<string> st;
-    while (fin3 >> s) {
-      st.insert(s);
-    }
-    fin3.close();
-    auto end_s = high_resolution_clock::now();
-    auto duration_s = duration_cast<microseconds>(end_s - start_s);
+    fin.close();
   }
 
   // Race 2: Sorting
-
-  // vector
-  auto start_sv = high_resolution_clock::now();
-  sort(v.begin(), v.end());
-  auto end_sv = high_resolution_clock::now();
-  auto vector_sort = duration_cast<microseconds>(end_sv - start_sv);
+  for (int i = 0; i < structs; i++) {
+    // vector
+    auto start = chrono::high_resolution_clock::now();
+    ifstream fin(file);
+    switch (i) {
+      case 0: {
+        while (fin >> s) {
+          // vector
+          auto start_sv = chrono::high_resolution_clock::now();
+          sort(v.begin(), v.end());
+          auto end_sv = chrono::high_resolution_clock::now();
+          auto vector_sort = duration_cast<microseconds>(end_sv - start_sv);
+        }
+      }
+      case 1: {
+        // list
+        while (fin >> s) {
+          l.push_back(s);
+          auto end = chrono::high_resolution_clock::now();
+          auto duration = duration_cast<microseconds>(end - start);
+          results[0][i][0] = duration.count();
+          break;
+        }
+      }
+      case 2: {
+        // set
+        while (fin >> s) {
+          st.insert(s);
+          auto end = chrono::high_resolution_clock::now();
+          auto duration = duration_cast<microseconds>(end - start);
+          results[0][i][0] = duration.count();
+          break;
+        }
+      }
+    }
+    fin.close();
+  }
 
   // list
-  auto start_sl = high_resolution_clock::now();
+  auto start_sl = chrono::high_resolution_clock::now();
   l.sort();
-  auto end_sl = high_resolution_clock::now();
+  auto end_sl = chrono::high_resolution_clock::now();
   auto list_sort = duration_cast<microseconds>(end_sl - start_sl);
 
   // set: already sorted by def
@@ -80,47 +114,47 @@ int main() {
   // Race 3: Insert
 
   // vector
-  auto start_vi = high_resolution_clock::now();
+  auto start_vi = chrono::high_resolution_clock::now();
   v.insert(v.begin() + (v.size() / 2), "TESTCODE");
   auto end_vi = high_resolution_clock::now();
   auto vector_insert = duration_cast<microseconds>(end_vi - start_vi);
 
   // list
-  auto start_li = high_resolution_clock::now();
+  auto start_li = chrono::high_resolution_clock::now();
   auto mid = l.begin();
   advance(mid, (l.size() / 2));
   l.insert(mid, "TESTCODE");
-  auto end_li = high_resolution_clock::now();
+  auto end_li = chrono::high_resolution_clock::now();
   auto list_insert = duration_cast<microseconds>(end_li - start_li);
 
   // set
-  auto start_si = high_resolution_clock::now();
+  auto start_si = chrono::high_resolution_clock::now();
   st.insert("TESTCODE");
-  auto end_si = high_resolution_clock::now();
+  auto end_si = chrono::high_resolution_clock::now();
   auto set_insert = duration_cast<microseconds>(end_si - start_si);
 
   // Race 4: Deleting
 
   // vector
-  auto start_vd = high_resolution_clock::now();
+  auto start_vd = chrono::high_resolution_clock::now();
   v.erase(v.begin() + (v.size() / 2));
-  auto end_vd = high_resolution_clock::now();
+  auto end_vd = chrono::high_resolution_clock::now();
   auto vector_delete = duration_cast<microseconds>(end_vd - start_vd);
 
   // list
-  auto start_di = high_resolution_clock::now();
+  auto start_di = chrono::high_resolution_clock::now();
   auto mid_d = l.begin();
   advance(mid_d, (l.size() / 2));
   l.erase(mid_d);
-  auto end_di = high_resolution_clock::now();
+  auto end_di = chrono::high_resolution_clock::now();
   auto list_delete = duration_cast<microseconds>(end_di - start_di);
 
   // set
-  auto start_sd = high_resolution_clock::now();
+  auto start_sd = chrono::high_resolution_clock::now();
   auto it_d = st.begin();
   advance(it_d, (st.size() / 2));
   st.erase(it_d);
-  auto end_sd = high_resolution_clock::now();
+  auto end_sd = chrono::high_resolution_clock::now();
   auto set_delete = duration_cast<microseconds>(end_sd - start_sd);
 
   // from lab 25
